00610362e151f02e9824f113df396c4e6124a58e
alonsomonterofuentes <alon182@gmail.com>
2016-04-24 23:08:21 -0600

    se agregaron comentarios a menu.py

--- menu.py
+++ menu.py
@@ -3,14 +3,14 @@ import os
 from time import sleep
 
 def header(opcion):
-		os.system('clear')
 		opcion = str(opcion)
 		l = len(opcion)	
+		print "\n"
 		print (l+4)*"*"
 		print 2*"*" +  str(opcion) + 2*"*"
 		print (l+4)*"*"
 
-def menu(lista):
+def opciones_menu(lista):
 		print "\n"
 		for item in lista:	
 				print str(lista.index(item)+1)+"."+item
@@ -24,5 +24,5 @@ def menu(lista):
 header("Bienvenido/a a la casa inteligente")
 sleep(1)
 header("Que desea hacer?")
-opciones_menu_principal = ["Configurar servidor","Ver info de sensores","Controlar actuador", "Salir"]
-menu(opciones_menu_principal)
+opciones_opciones_menu_principal = ["Configurar servidor","Ver info de sensores","Controlar actuador", "Salir"]
+opciones_menu(opciones_menu_principal)
