876df08a0cc51074b5aded72354b6e9f411f70d8
alonsomonterofuentes <alon182@gmail.com>
2016-04-25 11:13:23 -0600

    se hizo unala clase conf para poder accesarla de menu

--- conf.py
+++ conf.py
@@ -1,10 +1,13 @@
 import ConfigParser
-Config = ConfigParser.ConfigParser()
-Config.read("config.ini")
-def setAbanico(a,b,c):
-		Config.set(str(a),str(b),str(c))
-		print "se cambio el estado del " + str(a)
-setAbanico("Abanico","Estado","0")
-file = open("config.ini","w") #abre config.ini, si no existe la crea
-Config.write(file)#cierra el archivo
-file.close()
+class conf:
+		def __init__(self):
+				self.Config = ConfigParser.ConfigParser()
+				self.Config.read("config.ini")
+				self.setAbanico("Abanico","Estado","hola")
+				file = open("config.ini","w") 
+				self.Config.write(file)
+				file.close()
+		def setAbanico(self,a,b,c):
+				self.Config.set(str(a),str(b),str(c))
+				print "se cambio el estado del " + str(a)
+		
