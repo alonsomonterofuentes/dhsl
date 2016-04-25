4163b5a99468f76e2085e83382fcc30b63857655
alonsomonterofuentes <alon182@gmail.com>
2016-04-24 23:58:04 -0600

    Se corrigio la opcion que desplegaba cuando se elegia

--- menu.py
+++ menu.py
@@ -4,6 +4,7 @@ from time import sleep
 
 #crea un "banner" donde se pone un mensaje de manera que llame la atencion del usuario
 def banner(opcion):
+		os.system("clear")
 		opcion = str(opcion)
 		l = len(opcion)	
 		print "\n"
@@ -15,15 +16,15 @@ def opciones_menu(lista):
 		print "\n"
 		for item in lista:	
 				print str(lista.index(item)+1)+"."+item
-		x = raw_input("\nSeleccione una opcion\n")
+		x = raw_input("\nSeleccione una opcion:\n-Si es la primera vez que usa el programa, escoger la opcion 1")
 		x = int(x)
 		print "Se selecciono:"
-		print lista[x]
+		print lista[x-1]
 								
 
 
 banner("Bienvenido/a a la casa inteligente")
 sleep(1)
 banner("Que desea hacer?")
-opciones_opciones_menu_principal = ["Configurar servidor","Ver info de sensores","Controlar actuador", "Salir"]
+opciones_menu_principal = ["Configurar servidor","Ver info de sensores","Controlar actuador", "Salir"]
 opciones_menu(opciones_menu_principal)
