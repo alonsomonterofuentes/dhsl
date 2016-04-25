064f213a1d218e5c6a8fed6ad86fce60de11d627
alonsomonterofuentes <alon182@gmail.com>
2016-04-25 11:22:45 -0600

    se modifico para que se pueda modificar cualquier sensor o actuador

--- conf.py
+++ conf.py
@@ -1,13 +1,13 @@
 import ConfigParser
 class conf:
-		def __init__(self):
+		def __init__(self,a,b,c):
 				self.Config = ConfigParser.ConfigParser()
 				self.Config.read("config.ini")
-				self.setAbanico("Abanico","Estado","hola")
+				self.set(a,b,c)
 				file = open("config.ini","w") 
 				self.Config.write(file)
 				file.close()
-		def setAbanico(self,a,b,c):
+		def set(self,a,b,c):
 				self.Config.set(str(a),str(b),str(c))
 				print "se cambio el estado del " + str(a)
 		
