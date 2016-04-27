ee99af8e1032f429241a22835cac981c691e4241
alonsomonterofuentes <alon182@gmail.com>
2016-04-26 21:09:49 -0600

    se le agrego la funcion de configurar servidor, solo muestra las opciones

--- menu.py
+++ menu.py
@@ -1,33 +1,66 @@
 #!/usr/bin/env python
+
 from conf import *
 import os
 from time import sleep
 
+conf = conf(None,None,None)
+N = None
+def setN(n,mensaje):
+	global N
+	#manejo de errores, si lo que ingresa al usuario no puede ser convertido en un int vuelve a pedir que ingrese la opcion 
+	if n == None:
+		try:
+			N = int(raw_input(mensaje))
+		except ValueError:	
+			print "no es un numero, intentelo de nuevo."
+	else: 
+		N = n
+def getN():
+	global N	
+	return N
 #crea un "banner" donde se pone un mensaje de manera que llame la atencion del usuario
 def banner(opcion):
 		os.system("clear")
 		opcion = str(opcion)
 		l = len(opcion)	
-		print "\n"
+		#print "\n"
 		print (l+4)*"*"
 		print 2*"*" +  str(opcion) + 2*"*"
 		print (l+4)*"*"
 #Hace una lista con numeros para cada opcion de la lista
-def opciones_menu(lista):
-		print "\n"
+def opciones_menu(lista):	
 		for item in lista:	
 				print str(lista.index(item)+1)+"."+item
-		x = raw_input("\nSeleccione una opcion:\n-Si es la primera vez que usa el programa, escoger la opcion 1\n")
-		x = int(x)
-		print "Se selecciono:"
-		print lista[x-1]
-								
+		#x = raw_input("\nSeleccione una opcion:")
+		#x = int(x)
+		setN(None,"\nSeleccione una opcion:")
+#Definq que es lo que se hace al seleccionar algo en el menu principal
+def seleccion_principal(opcion):
+		if(opcion == 1 ):
+				configurar_servidor()
+def configurar_servidor():
+	banner("Configurar Servidor")
+	secciones = conf.Config.sections()
+	opciones_menu(secciones)
+	configurar_sec(secciones,getN())
 
-
-banner("Bienvenido/a a la casa inteligente")
-sleep(1)
-banner("Que desea hacer?")
-opciones_menu_principal = ["Configurar servidor","Ver info de sensores","Controlar actuador", "Salir"]
-opciones_menu(opciones_menu_principal)
-x = conf("Abanico","estado","1")
+def configurar_sec(secciones,seccion):
+	banner("Configurar "+secciones[seccion])	
+	opciones_menu(conf.Config.options(secciones[seccion]))
+	
+def principal():
+	#ensena el banner de bienvenido por medio segundo
+	banner("Bienvenido/a a la casa inteligente")
+	sleep(0.5)
+	#ensena el banner de el menu principal
+	banner("Que desea hacer?")
+	print "-Si es la primera vez que usa el programa, escoger la opcion 1\n"
+	#estas son las opciones del menu principal, se dan como un vector a opciones_menu para que las despliegue
+	opciones_menu_principal = ["Configurar servidor(editor)","Configurar servidor(Nano)","Ver info de sensores","Controlar actuador", "Volver"]
+	opciones_menu(opciones_menu_principal)
+	seleccion_principal(getN())
+	#confWrite.set("Abanico","estado","2")
+	#print confWrite.Config.get("Abanico","estado")
+principal()
 
