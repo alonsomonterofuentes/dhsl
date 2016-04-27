2e49917f6ed3254d88b8bfa45acdd7029a8d6061
alonsomonterofuentes <alon182@gmail.com>
2016-04-26 21:08:54 -0600

    se modifico para que se puedieran accesar los datos sin que se borrara todo

--- conf.py
+++ conf.py
@@ -3,11 +3,14 @@ class conf:
 		def __init__(self,a,b,c):
 				self.Config = ConfigParser.ConfigParser()
 				self.Config.read("config.ini")
-				self.set(a,b,c)
-				file = open("config.ini","w") 
-				self.Config.write(file)
-				file.close()
+				if(a != None):
+					print "a no es None"
+					self.set(a,b,c)
+					file = open("config.ini","w") 
+					self.Config.write(file)
+					file.close()
+
+				#file.close()
 		def set(self,a,b,c):
 				self.Config.set(str(a),str(b),str(c))
 				print "se cambio el estado del " + str(a)
-		
