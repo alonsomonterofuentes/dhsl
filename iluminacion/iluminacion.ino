f9c7ff3de09e21584195de05bee6deb3b23b769d
alonsomonterofuentes <alon182@gmail.com>
2016-05-03 17:22:45 -0600

    se corrigieron los nombres de las funciones

--- iluminacion/iluminacion.ino
+++ iluminacion/iluminacion.ino
@@ -1,79 +1,107 @@
-const int led = 13;
-const int fotoresistor = A0;
-int valorFotoresistor = 0;
-int i;
-int valorMaxInt;
-char inputServer[8];
-char valorMaxChar[4];
-String x;
- //para ultrasonico
+/*
+*El protocolo de comunicacion entre el arduino y el servidor consiste en enviar y recibir
+*Strings que son descompuestas en componentes[caracteres] que representan datos importantes para el programa
+*Podemos representarlos asi
+*[0] -> tiene valores de 1(encendido) o 0(apagado), representa el control manual
+*[1] -> tiene valores de 1(encendido) o 0(apagado), representa el estado del led
+*[2] -| 
+*[3]  |-> unidos nos dan un valor entre 000 y 999, representanda el valor maximo de luminosidad
+*[4] -|
+*[5] -|
+*[6]  |-> unidos nos dan un valor entre 000 y 999, representa el valor la sensibilidad del sensor ultrasonico(cm) 
+*[7] -|
+**[\0]
+*/
+const int led = 13; 
 const int  echo = 11;
 const int trig = 12;
+const int fotoresistor = A0;
 long duracion; 
 long distancia;
-char valorSensibilidadChar[4];
+int i;
+int valorMaxInt; 
 int valorSensibilidadInt;
+String lecturaSerial;
+char serverInput[10];
+char serverOutput[8]; 
+char valorMaxChar[4];
+char valorSensibilidadChar[4];
 void setup(){
-  
- pinMode(led, OUTPUT) ;// Se declara el pin como salida
- Serial.begin(9600);// Configuracion de la conexion serial y velocidad
- pinMode(fotoresistor, INPUT); // Se declara el pin como entrada
- pinMode(echo, INPUT);     // declara el pin como entrada
- pinMode(trig, OUTPUT);    // dvlara el pin como salida
-}
-
-void ctrl_light(int a){
-  valorFotoresistor = analogRead(fotoresistor);
-  if (valorFotoresistor < a ){
-    digitalWrite(led, HIGH);    
-  }
-  else {
-    digitalWrite(led, LOW);
-  }
-}
-
-boolean hayMovimiento(int sensibilidad){
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);   // genera el pulso de triger por 10ms
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  duracion = pulseIn(echo, HIGH);
-  distancia = (duracion/2) / 29;            // calcula la distancia en centimetros
-  if(distancia < sensibilidad){
-    return true;
-  }//terminad if distancia<sensibilidad
-  else{return false;}
-}
-
+		pinMode(led,OUTPUT);
+		pinMode(fotoresistor,INPUT);
+		Serial.begin(9600);
+}//termina setup
+int lecturaFotoresistor(){
+		return analogRead(fotoresistor);
+}//termina lecturaFotoresistor
+int lecturaUltrasonico(){
+		digitalWrite(trig, LOW);
+		delayMicroseconds(2);
+		digitalWrite(trig, HIGH);// genera el pulso de triger por 10ms
+		delayMicroseconds(10);
+		digitalWrite(trig, LOW);
+		duracion = pulseIn(echo, HIGH);
+		distancia = (duracion/2) / 29;	// calcula la distancia en centimetros
+		return distancia;
+}//termina lecturaUltrasonico
+boolean hayMovimiento(int distanciaLeida, int distanciaEstablecida){
+		if(distanciaLeida<distanciaEstablecida){
+				return true;
+		}
+		else{
+				return false;
+		}
+}//termina hayMovimiento
+void encenderLed(){ 
+		digitalWrite(led, HIGH); 
+}//termina encenderLed
+void apagarLed(){
+		digitalWrite(led,LOW); 
+}//termina apagarLed
+void recibirDatos(){
+		while(Serial.available()){
+				lecturaSerial=Serial.readString();          //recibe un string por medio de serial
+				lecturaSerial.toCharArray(serverInput,9);   //convierte ese string en char de 9
+				valorMaxChar[0] = serverInput[2];	    //la tercera entrada se sI es la primera de mL
+				valorMaxChar[1] = serverInput[3];	    //lo mismo sucede para esta 
+				valorMaxChar[2] = serverInput[4];	    //y para esta
+				valorMaxInt = atoi(valorMaxChar);
+				valorSensibilidadChar[0] = serverInput[5];
+				valorSensibilidadChar[1] = serverInput[6];
+				valorSensibilidadChar[2] = serverInput[7];
+				valorSensibilidadInt = atoi(valorSensibilidadChar);
+		}	//termina while serial.available	
+}//termina recibirDatos
+boolean controlManualActivado(char inputUsuario){
+		if(inputUsuario == '1'){return true;}
+		else if(inputUsuario == '0'){return false;}
+}//termina control manual activado
+void controlManual(){
+		if(controlManualActivado(serverInput[0])==true){	
+				if(serverInput[1]=='0'){
+						apagarLed();
+				}
+				else if(serverInput[1] == '1'){
+						encenderLed();	
+				}				
+		}
+		//(controlManualActivado(serverInput[0])==false){
+		else if(controlManualActivado(serverInput[0])==false){controlAutomatico();}
+}//termina control manual
+void controlAutomatico(){
+						while(hayMovimiento(lecturaUltrasonico(),valorSensibilidadInt) == true && Serial.available()==false){			
+								if(valorMaxInt<lecturaFotoresistor()){
+										encenderLed();
+								}//termina if	
+								else {
+										apagarLed();
+								}//termina else
+		}//termina while
+}//termina control automatico
 void loop(){
- while(Serial.available()){
-    x = Serial.readString();
-    x.toCharArray(inputServer,9);
-    valorMaxChar[0] = inputServer[2];
-    valorMaxChar[1] = inputServer[3];
-    valorMaxChar[2] = inputServer[4];
-    valorMaxInt = atoi(valorMaxChar);
-    valorSensibilidadChar[0] = inputServer[5];
-    valorSensibilidadChar[1] = inputServer[6];
-    valorSensibilidadChar[2] = inputServer[7];
-    valorSensibilidadInt = atoi(valorSensibilidadChar);
-    while(inputServer[0]=='0'){ //pide control manual apagado
-      if(Serial.available()){
-      break;
-      }//termina if serialavailable
-      while(hayMovimiento(valorSensibilidadInt) == true){
-         ctrl_light(valorMaxInt);     
-      }//termina while haymovimiento ==true
-      while(hayMovimiento(valorSensibilidadInt) == false){digitalWrite(led,LOW);}
-    }//termina while [0] = '0'
-    if(inputServer[0]=='1'){ //pide control manual encendido
-      if(inputServer[1]=='0'){
-         digitalWrite(led, LOW);
-      }//termina if estado apagado
-      else if(inputServer[1]=='1'){
-         digitalWrite(led, HIGH);
-      }//termina if estado apagado
-    }//termina if control manual
-  }//termina while serialavailable
-}//termina loop
+		recibirDatos();
+		if(hayMovimiento(lecturaUltrasonico(),valorSensibilidadInt)==true){Serial.println('1');}
+		if(hayMovimiento(lecturaUltrasonico(),valorSensibilidadInt)==false){Serial.println('0');}
+		controlManual();
+		}//termina loop	
+
