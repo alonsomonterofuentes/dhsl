f02373ec097bb6fc19797f9720dcefd17b4957e5
alonsomonterofuentes <alon182@gmail.com>
2016-05-08 11:02:16 -0600

    se oredeno un poco mas y se comento mejor

--- menu.py
+++ menu.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 from escribirSerial import *
-from lectura import *
 from conf import *
 import os
 from time import sleep
 N = None
 conf = conf(None,None,None)
+
+#pide un input al usuario
 def setN(n,mensaje):
 		global N
 		if n == None:
+		#manejo de errores(si no es un numero, lop pide de nuevo)
 				try:
 						N = int(raw_input(mensaje))
 				except ValueError:	
 						setN(None,"no es un numero, intentelo de nuevo.")
 		else: 
 				N = n
+
+#devuelve el input del usuario
 def getN():
 		global N	
 		return N
+
 #crea un "banner" donde se pone un mensaje de manera que llame la atencion del usuario
 def banner(opcion):
 		os.system("clear")
@@ -27,24 +32,24 @@ def banner(opcion):
 		print (l+4)*"*"
 		print 2*"*" +  str(opcion) + 2*"*"
 		print (l+4)*"*"
-#Hace una lista con numeros para cada opcion de la lista
+
+#Hace una lista con opciones numeradas para cada entrada de la lista
 def opciones_menu(lista):	
 		for item in lista:	
 				print str(lista.index(item)+1)+"."+item
 		setN(None,"\nSeleccione una opcion:")
+
 #Define que es lo que se hace al seleccionar algo en el menu principal
 def seleccion_principal(opcion):
 		if(opcion == 1 ):
 			configurar_servidor()
-		elif(opcion ==2):
-				controlarBombillo()	
-		elif(opcion == 3):
-				contrloarVentilador
-		elif(opcion == 4):
+		elif(opcion == 2):
 			tomarFotos()
+		elif(opcion ==3):
+				es = escribirSerial(conf.Config.get("ArduinoUno","puerto"),conf.Config.get("ArduinoDos","puerto"))
 		elif(opcion ==4):
-			os.system("clear")
 			exit()
+
 #menu de configurar servidor
 def configurar_servidor():
 		banner("Configurar Servidor")
@@ -68,41 +73,14 @@ def configurar_sec(secciones,seccion):
 		setN(None,"Nuevo valor de configuracion manual de " + opcionElegida + " ")
 		conf.set(str(secciones[seccion]),str(opcionElegida),getN())
 		principal()
-#muestra los valores de los sensores por aparte
+
+#permite al usuario tomar una foto y escoger el nombre de esta
 def tomarFotos():
 		n = raw_input("Nombre de la imagen")
 		os.system("fswebcam -r 1920x1080 " + str(n) +".jpg")
 		principal()
-def controlarBombillo():
-		banner("Desea activar el control manual del Bombillo?")
-		siono = ["si","no"]
-		apagaroEncender=["Apagar","Encender"]
-		opciones_menu(siono)
-		if(getN()==1):
-				banner("Desea apagar o encender el bombillo?")
-				opciones_menu(apagaroEncender)
-				if getN()==1:
-						conf.Config.set("Iluminacion","Control Manual","1")
-						conf.Config.set("Bombillo","estado","0")
-				elif getN() == 2:
-						conf.Config.set("Iluminacion","Control Manual","1")
-						conf.Config.set("Bombillo","estado","1")
-		else: principal()
-def controlarVentilador():
-		banner("Desea activar el control manual del Ventilador?")
-		siono = ["si","no"]
-		apagaroEncender=["Apagar","Encender"]
-		opciones_menu(siono)
-		if(getN()==1):
-				banner("Desea apagar o encender el ventilador?")
-				opciones_menu(apagaroEncender)
-				if getN()==1:
-						conf.Config.set("Temperatura","Control Manual","1")
-						conf.Config.set("Abanico","estado","0")
-				elif getN() == 2:
-						conf.Config.set("Temperatura","Control Manual","1")
-						conf.Config.set("Abanico","estado","1")
-		else: principal()
+
+#funcion principal
 def principal():
 		#ensena el banner de bienvenido por medio segundo
 		banner("Bienvenido/a a la casa inteligente")
@@ -111,10 +89,12 @@ def principal():
 		banner("Que desea hacer?")
 		print "-Si es la primera vez que usa el programa, escoger la opcion 1\n"
 		#estas son las opciones del menu principal, se dan como un vector a opciones_menu para que las despliegue
-		opciones_menu_principal = ["Configurar servidor","Controlar Bombillo","Controlar Ventilador","Tomar foto", "Salir"]
+		opciones_menu_principal = ["Configurar servidor","Tomar foto","Escribir a arduino", "Salir"]
 		opciones_menu(opciones_menu_principal)
 		seleccion_principal(getN())
-		eS = escribirSerial()
-principal()
+
+#corre la funcion principal en un ciclo infinito
+while True:
+		principal()
 		
 
