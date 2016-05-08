eacb5cfcec5cbcb9cd48414a5c986870451d948b
alonsomonterofuentes <alon182@gmail.com>
2016-05-08 11:06:12 -0600

    se ordeno y se comento mejor

--- conf.py
+++ conf.py
@@ -1,16 +1,19 @@
 import ConfigParser
+
 class conf:
+		#inicializa el archivo config.ini, si tiene argumentos corre la funcion set
 		def __init__(self,a,b,c):
 				self.Config = ConfigParser.ConfigParser()
 				self.Config.read("config.ini")
 				if(a != None):
-					print "a no es None"
-					self.set(a,b,c)
-					file = open("config.ini","w") 
-					self.Config.write(file)
-					file.close()
-
-				#file.close()
+						self.set(a,b,c)
+						file = open("config.ini","w") 
+						self.Config.write(file)
+						file.close()
+		#escribe al archivo config.ini
+		
 		def set(self,a,b,c):
 				self.Config.set(str(a),str(b),str(c))
+				file = open("config.ini","w") 
+				self.Config.write(file)
 				print "se cambio el estado del " + str(a)
